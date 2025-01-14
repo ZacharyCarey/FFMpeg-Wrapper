25f82e1142cac17a100cde49cd3d2095723a4061
Zachary Carey <zach.d.carey@gmail.com>
2025-01-14 14:27:36 -0500

    Added anullsrc input

---
+++ FFMpeg-Wrapper/ffmpeg/AudioNullSrcInput.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FFMpeg_Wrapper.ffmpeg {
+
+    /// <summary>
+    /// Creates an "anullsrc" audio input. This acts as an empty/null audio source input.
+    /// </summary>
+    public class AudioNullSrcInput : FFMpegInput {
+        private readonly TimeSpan Duration;
+        TimeSpan FFMpegInput.Duration => this.Duration;
+
+        void IFFMpegArgs.Begin() {
+            
+        }
+
+        void IFFMpegArgs.Cleanup() {
+            
+        }
+
+        IEnumerable<string> IFFMpegArgs.GetArguments() {
+            yield return "-f lavfi";
+            yield return $"-t {Duration.GetFFMpegFormat()}";
+            yield return "-i anullsrc";
+        }
+
+        public AudioNullSrcInput(TimeSpan duration) {
+            this.Duration = duration;
+        }
+    }
+}
